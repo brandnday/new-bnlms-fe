d9ed0360df14e1522d9d4f5ea324e7210e9af675
Bryan <brandnday@gmail.com>
2019-08-05 09:46:01 +0700

    redux admin

--- pages/admin-manager/index.js
+++ pages/admin-manager/index.js
@@ -1,109 +1,24 @@
 import React from 'react'
-
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware,combineReducers } from 'redux';
+import thunk from 'redux-thunk';
 import 'antd/dist/antd.css';
 import DashboardWrapperPage from '../../components/shared/DashboardWrapperPage';
-import { generateColumns } from '../../tools/generators';
-import AdminModal from '../../components/admin-manager/AdminModal'
-import {
-  Button, Input, Col, Table,
-} from 'antd';
-import Select from '../../components/shared/Select'
-const InputGroup = Input.Group;
+import AdminManagerReducer from '../../actions-redux/AdminManager/AdminManagerReducers'
+import AdminManagerPageContent from '../../components/admin-manager/AdminManagerPageContentContainer.js'
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-const data = [{ name: 'test', role: 'admin', status: 'active' }];
-const roleData = [{ id: 0, text: 'All' }, { id: 1, text: 'super admin' }]
-export default class SiderDemo extends React.Component {
-  state = {
-    visible: false,
-    editdata:1
-  }
-
-  handleOk = e => {
-    this.setState({
-      visible: false
-    });
-  };
-
-  handleCancel = e => {
-    this.setState({
-      visible: false
-    });
-  };
-
-  showModal = (editid, name) => {
-    this.setState({
-      visible: true,
-      editdata:{id:editid}
-    });
-  };
-  columns = [
-    ...generateColumns([
-      { title: 'Name', key: 'name' },
-      { title: 'Role', key: 'role' },
-      { title: 'Status', key: 'status' }
-    ]),
-    {
-      title: 'Action',
-      key: 'id',
-      dataIndex: 'id',
-      render: (text, record) => (
-        <span>
-        <a onClick={() => this.showModal(1, '')}>
-          update</a>
-          <a onClick={() => this.handleDelete(text)}>
-            Delete</a>
-        </span>
-      )
-    }]
+const store = createStore(
+  combineReducers({AdminManagerReducer: AdminManagerReducer}),
+  composeWithDevTools(applyMiddleware(thunk)))
+export default class AdminManagerPage extends React.Component {
   render() {
     return (
+      <Provider store={store}>
       <DashboardWrapperPage title='Dashboard' subtitle=''>
-        <div>
-
-          <InputGroup size='large'>
-            <Col span={5}>
-              <Input placeholder="Search name" />
-            </Col>
-            <Col span={5}>
-              <Select
-                onChange={() => console.log("asdf")}
-                size='large'
-                style={{ width: 200 }}
-                value={0}
-                placeholder='Select role'
-                options={roleData}
-              />
-            </Col>
-            <Col span={5}>
-              <Button
-                type='primary'
-                icon='add'
-                size='large'
-                onClick={value => {
-                  this.showModal(-1, '');
-                }}
-              >Insert New User
-            </Button>
-            </Col>
-          </InputGroup>
-          <Table
-            columns={this.columns}
-            dataSource={data}
-            pagination={false}
-            loading={false}
-          />
-
-          <AdminModal
-            title="Insert New Admin"
-            visible={this.state.visible}
-            footer={null}
-            onCancel={this.handleCancel}
-            onSubmit={this.handleSubmit}
-            editingid = {this.state.editdata.id}
-          />
-
-        </div>
+        <AdminManagerPageContent />
       </DashboardWrapperPage>
+      </Provider>
     );
   }
 }
